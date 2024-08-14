f96373f1eceda2cd2e72d76a3548ed2cc6584789
Ludovit <kopcsanyiludovit@gmail.com>
2024-08-15 00:31:42 +0200

    Fixed input styles

--- src/app/components/calendar/BookingForm.tsx
+++ src/app/components/calendar/BookingForm.tsx
@@ -1,18 +1,40 @@
+import { ChangeEvent, useState } from "react";
 import Input from "../form/Input";
 
 const BookingForm = () => {
+
+	const [ name, setName ] = useState<string>('')
+	const [ phone, setPhone ] = useState<string>('')
+	const [ email, setEmail ] = useState<string>('')
+	const [ note, setNote ] = useState<string>('')
+
+	const handleNameChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setName(event.target.value)
+	}
+
+	const handlePhoneChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setPhone(event.target.value)
+	}
+
+	const handleEmailChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setEmail(event.target.value)
+	}
+
+	const handleNoteChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setNote(event.target.value)
+	}
 	
 	return (
 		<form className="max-w-md mx-auto mt-10">
 			<div className="mb-4">
-				<Input className='bg-gray-200' label="Meno a priezvisko" type="text" />
+				<Input className='bg-gray-200' label="Meno a priezvisko" type="text" value={ name } onChange={ handleNameChange } />
 			</div>
 			<div className="mb-4 flex gap-x-4">
-				<Input inline={ true } className='w-1/2 bg-gray-200' label="Telefónne číslo" type="tel" />
-				<Input inline={ true } className='w-1/2 bg-gray-200' label="E-mail" type="email" />
+				<Input inline={ true } className='w-1/2 bg-gray-200' label="Telefónne číslo" type="tel" value={ phone } onChange={ handlePhoneChange }/>
+				<Input inline={ true } className='w-1/2 bg-gray-200' label="E-mail" type="email" value={ email } onChange={ handleEmailChange }/>
 			</div>
 			<div className="mb-4">
-				<textarea className="w-full px-3 py-2 border border-gray-300 bg-gray-200 rounded-md" placeholder="Poznámka"></textarea>
+				<textarea className="w-full px-3 py-2 border border-gray-300 bg-gray-200 rounded-md" placeholder="Poznámka" value={ note } onChange={ handleNoteChange }></textarea>
 			</div>
 			<div className="text-center">
 				<button className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Potvrdiť rezerváciu</button>
--- src/app/components/form/Input.tsx
+++ src/app/components/form/Input.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ReactNode } from "react"
+import { ChangeEvent, ReactNode, useState } from "react"
 
 type Props = {
 	id?: string,
@@ -50,13 +50,13 @@ const Input = ({ id, label, type, inline, icon, className, placeholder, value, o
 					htmlFor={ id }
 					className={
 						`absolute text-sm text-gray-500 dark:text-gray-400
-						duration-300 transform -translate-y-4 scale-75 top-4 z-10
+						duration-300 z-10
 						origin-[0] start-2.5
 						peer-focus:text-blue-600 peer-focus:dark:text-blue-500
 						peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
-						peer-focus:scale-75 peer-focus:-translate-y-4
+						peer-focus:scale-75 peer-focus:-translate-y-0 peer-focus:top-0
 						rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto z-0
-						cursor-text`
+						cursor-text ${value ? 'scale-75 -translate-y-0 top-0' : 'top-1/2 -translate-y-1/2'}`
 					}
 				>
 					{ label }
